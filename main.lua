e13890110e2d1cec576301f94df4a6de76f4113b
omnimistic <prasparadise@gmail.com>
2026-06-28 17:38:20 +0530

    can paint now

--- main.lua
+++ main.lua
@@ -1,11 +1,21 @@
-function love.load()
 
+local brush_strokes = {}
+
+function love.load()
+    love.graphics.setBackgroundColor(1,1,1)
 end
 
 function love.update(dt)
-
+    if love.mouse.isDown(1) then
+        local mouse_x, mouse_y = love.mouse.getPosition()
+        table.insert(brush_strokes, {x = mouse_x, y = mouse_y})
+    end
 end
 
 function love.draw()
-
+    love.graphics.setColor(0, 0, 0)
+    
+    for index, point in ipairs(brush_strokes) do
+        love.graphics.circle("fill", point.x, point.y, 3)
+    end
 end
